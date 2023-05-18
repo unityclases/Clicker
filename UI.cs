a0dffc8d7da16030d46adfa114d182098e1d530b
unityclases <126904973+unityclases@users.noreply.github.com>
2023-05-18 11:27:25 +0200

    Create UI.cs

---
+++ UI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Text scoreText;
+    static UI singleton;
+    public GameObject panel;
+    public Text panelScoreText;
+    public Text defeatText;
+    public Text victoryText;
+
+    void Awake()
+    {
+        singleton = this;
+    }
+
+    public static void ShowVictoryPanel()
+    {
+        singleton.panel.SetActive(true);
+        singleton.victoryText.gameObject.SetActive(true);
+        singleton.panelScoreText.text = Player.score.ToString();
+    }
+
+    public static void ShowDefeatPanel()
+    {
+        singleton.panel.SetActive(true);
+        singleton.defeatText.gameObject.SetActive(true);
+        singleton.panelScoreText.text = Player.score.ToString();
+    }
+
+
+    private void Update()
+    {
+        scoreText.text = Player.score.ToString();
+    }
+
+    public void OnClickRestart()
+    {
+        Player.Restart();
+    }
+}
